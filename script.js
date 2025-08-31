712de6e3b0d86fd4d96f842c746688d4bbd7620e
Abhinav M <130998983+abhinav0700@users.noreply.github.com>
2025-08-31 18:54:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 	let freqs = {}
-for(let i=0;i<str.length;i++){
-    freqs[str[i]] = (freqs[str[i]]||0)+1;
+for(let num of str){
+    freqs[num] = (freqs[num]||0)+1;
 }
 for(let num of str){
     if (freqs[num] === 1){
