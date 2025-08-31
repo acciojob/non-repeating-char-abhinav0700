1a697037c87e171d2fe8c7397a557b1236cbc7fe
Abhinav M <130998983+abhinav0700@users.noreply.github.com>
2025-08-31 18:51:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let freqs = {}
+for(let i=0;i<str.length;i++){
+    freqs[str[i]] = (freqs[str[i]]||0)+1;
+}
+for(let num of str){
+    if (freqs[num] === 1){
+        return num;
+    }
+}
+return null;
+}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
